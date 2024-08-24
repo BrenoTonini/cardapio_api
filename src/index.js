1809fadb0de18085adc5e4fc35873acfca0ff318
BrenoTonini <brenotoninicosta4@hotmail.com>
2024-08-23 23:26:25 -0300

    fix cors issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors'); // Importa o CORS
 const loginRoute = require('./routes/login');
 
 const app = express();
+
+// Configura o CORS para permitir requisições de http://localhost:5173
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 
 app.use('/api', loginRoute);
 
-app.listen(); 
+app.listen();
