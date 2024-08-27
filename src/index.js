7fe18615db9ac5f7d539cfe43dd047bd2603fed7
BrenoTonini <brenotoninicosta4@hotmail.com>
2024-08-27 12:26:54 -0300

    trying to fix cors issue x8

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const loginRoute = require('./routes/login');
 const sessionRoute = require('./routes/session');
 const filesRoute = require('./routes/files');
 const uploadRoute = require('./routes/upload');
-const allowCors = require('./middlewares/allowCors');
 const authenticateToken = require('./middlewares/authenticateToken');
 
 const app = express();
 
-//habilit
-app.use(allowCors);
+app.use(cors({
+  origin: 'http://projeto-midia-indoor-navy.vercel.app'
+}));
 app.use(express.json());
 
 //rotas que não precisam do auth token
